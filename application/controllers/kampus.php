b5c4a82c91c7e60daa12ce2ee5f38cfc0e962e60
sitihana123 <sitihanahan56@gmail.com>
2022-10-13 20:47:45 +0700

    merubah tampilan

--- application/controllers/kampus.php
+++ application/controllers/kampus.php
@@ -1,56 +1,84 @@
 <?php
 class Kampus extends CI_Controller{
 
-    function __construct() {
-        parent::__construct();
+    function __construct(){
+        parent ::__construct();
         $this->load->model('m_data');
         $this->load->helper('url');
     }
 
-    function index() {
+    function index(){
         $data['mahasiswa'] = $this->m_data->tampil_data()->result();
-        $this->load->view('tampil_data', $data);
+        $this->load->view('tampil_data',$data);
     }
 
-    function tambah() {
+    function tambah(){
         $this->load->view('input_data');
     }
 
-    function tambah_aksi() {
+    function tambah_aksi(){
         $nim = $this->input->post('nim');
         $nama = $this->input->post('nama');
         $alamat = $this->input->post('alamat');
         $pekerjaan = $this->input->post('pekerjaan');
 
-        $data = array(
-            'nim' => $nim,
-            'nama' => $nama,
-            'alamat' => $alamat,
-            'pekerjaan' => $pekerjaan
-        );
+        $config['max_size']=2048;
+        $config['allowed_types']="png|jpg|jpeg|gif";
+        $config['remove_spaces']=TRUE;
+        $config['overwrite']=TRUE;
+        $config['upload_path']=FCPATH.'images';
 
-        $this->m_data->input_data($data,'mahasiswa');
-        redirect('kampus/index');
-    }
+        $this->load->library('upload');
+        $this->upload->initialize($config);
 
-    function edit($id) {
-        $where = array('id' => $id);
-        $data['mahasiswa'] = $this->m_data->edit_data($where,'mahasiswa')->result();
-        $this->load->view('edit_data',$data);
-    }
-
-    function update() {
-        $id = $this->input->post('id');
-        $nim = $this->input->post('nim');
-        $nama = $this->input->post('nama');
-        $alamat = $this->input->post('alamat');
-        $pekerjaan = $this->input->post('pekerjaan');
+        $this->upload->do_upload('foto');
+        $data_image=$this->upload->data('file_name');
+        $location='images/';
+        $foto=$location.$data_image;
 
         $data = array(
             'nim' => $nim,
             'nama' => $nama,
             'alamat' => $alamat,
             'pekerjaan' => $pekerjaan,
+            'foto' => $foto
+            );
+        $this->m_data->input_data($data,'mahasiswa');
+        redirect('kampus/index');
+    }
+    function edit($id) {
+        $where = array('id' => $id);
+        $data['mahasiswa'] = $this->m_data->edit_data($where,'mahasiswa')->result();
+        $this->load->view('edit_data',$data);
+    }
+
+    function update(){
+        $id =$this->input->post('id');
+        $nim =$this->input->post('nim');
+        $nama =$this->input->post('nama');
+        $alamat =$this->input->post('alamat');
+        $pekerjaan =$this->input->post('pekerjaan');
+
+        $config['max_size']=2048;
+        $config['allowed_types']="png|jpg|jpeg|gif";
+        $config['remove_spaces']=TRUE;
+        $config['overwrite']=TRUE;
+        $config['upload_path']=FCPATH.'images';
+
+        $this->load->library('upload');
+        $this->upload->initialize($config);
+
+        $this->upload->do_upload('foto');
+        $data_image=$this->upload->data('file_name');
+        $location='images/';
+        $foto=$location.$data_image;
+
+        $data = array(
+            'nim' => $nim,
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'pekerjaan' => $pekerjaan,
+            'foto' => $foto
         );
 
         $where = array(
@@ -61,9 +89,15 @@ class Kampus extends CI_Controller{
         redirect('kampus/index');
     }
 
-    function hapus($id) {
-        $where = array('id' => $id);
-        $this->m_data->hapus_data($where,'mahasiswa');
-        redirect('kampus/index');
+        function hapus($id) {
+            $where = array('id' => $id);
+            $this->m_data->hapus_data($where,'mahasiswa');
+            redirect('kampus/index');
+        }
     }
-}
+
+
+
+
+
+        
